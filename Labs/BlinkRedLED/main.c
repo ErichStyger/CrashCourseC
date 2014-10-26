1d83104c06c2d283549fe7914aa27e3fc8dc1cfa
Erich Styger <erich.styger@hslu.ch>
2014-10-26 17:25:38 +0100

    added first labs

--- Labs/BlinkRedLED/main.c
+++ Labs/BlinkRedLED/main.c
@@ -1,10 +1,14 @@
 #include "Cpu.h"  /* interface to CPU, do not removed  */
+#include "WAIT1.h"  /* waiting routines  */
+#include "LEDR.h"   /* red LED */
 
 int main(void) {
   PE_low_level_init(); /* low level driver initialization, do not remove */
 
   for(;;) {
-    /* add your code here */
+     /* add your code here */
+     LEDR_Neg();
+     WAIT1_Waitms(500);
   }
   /* do not leave main! */
   return 0;
